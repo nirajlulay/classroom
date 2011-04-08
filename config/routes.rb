976e52918cce8a282c7756b019cf17e1352ce1aa
niraj <nirajlulay@csu.fullerton.edu>
2011-04-08 00:00:07 -0700

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Classroom::Application.routes.draw do
-  root :to => "login#index"
-  resources :charts do
-resources :comments
-end
+  root :to => "sessions#new"
+  resources :charts 
   resources :users
   resource :session
   match '/login' => "sessions#new", :as => "login"
